1394d9e3d5c76a840ac8b23eb46466dae1533fbc
Casey Klebba <cklebba@netflix.com>
2018-09-06 17:11:07 -0700

    Exposes all directives in lit-html

--- x-element.js
+++ x-element.js
@@ -9,17 +9,36 @@
 
 import XElementProperties from './x-element-properties.js';
 import { render, html } from '../../lit-html/lit-html.js';
+
+import { asyncAppend } from '../../lit-html/directives/async-append.js';
+import { asyncReplace } from '../../lit-html/directives/async-replace.js';
+import { guard } from '../../lit-html/directives/guard.js';
+import { ifDefined } from '../../lit-html/directives/if-defined.js';
 import { repeat } from '../../lit-html/directives/repeat.js';
+import { unsafeHtml } from '../../lit-html/directives/unsafe-html.js';
+import { until } from '../../lit-html/directives/until.js';
+import { when } from '../../lit-html/directiveswhen.js';
+
+const directives = {
+  asyncAppend,
+  asyncReplace,
+  guard,
+  ifDefined,
+  repeat,
+  unsafeHtml,
+  until,
+  when,
+};
 
 export default class AbstractElement extends XElementProperties {
   render() {
-    const tmpl = this.constructor.template(html, repeat);
+    const tmpl = this.constructor.template(html, directives);
     const proxy = this.constructor.renderProxy(this);
     render(tmpl(proxy, this), this.shadowRoot);
   }
 
   /* eslint-disable no-shadow, no-unused-vars */
-  static template(html, repeat) {
+  static template(html, directives) {
     return (proxy, original) => html``;
   }
 }
