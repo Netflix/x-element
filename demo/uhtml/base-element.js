0ff792924ef5fdc0e250c21478ba475855066e23
Andrew Seier <andseier@gmail.com>
2024-02-01 13:20:45 -0800

    Update import paths for networked demos.
    In a some places, we demo loading 3rd party dependencies over-the-wire.
    The latest versions of these dependencies on `unpkg` require a slightly
    different url. Just a minor change to update those.

--- demo/uhtml/base-element.js
+++ demo/uhtml/base-element.js
@@ -1,5 +1,5 @@
 import XElement from '../../x-element.js';
-import { render, html, svg } from 'https://unpkg.com/uhtml?module';
+import { render, html, svg } from 'https://unpkg.com/uhtml';
 
 export default class BaseElement extends XElement {
   // Use µhtml's template engine rather than the built-in x-element engine.
