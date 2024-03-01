03e1cf7ba720c5ae419f65938df1c4cdaa8f5693
Andrew Seier <andseier@gmail.com>
2024-02-29 17:22:18 -0800

    Bump version for “x-test” development dependency.

--- test/x-test.js
+++ test/x-test.js
@@ -1,3 +1,3 @@
 // Simply import / export so that we pin this version in one spot.
-import { test, describe, it, waitFor, assert, coverage } from 'https://unpkg.com/@netflix/x-test@1.0.0-rc.20/x-test.js';
+import { test, describe, it, waitFor, assert, coverage } from 'https://unpkg.com/@netflix/x-test@1.0.0/x-test.js';
 export { test, describe, it, waitFor, assert, coverage };
