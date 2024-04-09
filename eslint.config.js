eacef3bedbdbfebe2d674265e1fed319ebd25550
Casey Klebba <casey@klebba.net>
2024-04-09 15:55:53 -0700

    Adopts shared linter rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,11 @@
 import globals from 'globals';
-import js from '@eslint/js';
-
-const common = {
-  rules: {
-    ...js.configs.recommended.rules,
-    'comma-dangle': ['warn', {
-      arrays: 'always-multiline',
-      objects: 'always-multiline',
-      imports: 'always-multiline',
-      exports: 'always-multiline',
-      functions: 'only-multiline',
-    }],
-    'eqeqeq': 'error',
-    'no-console': 'warn',
-    'no-prototype-builtins': 'warn',
-    'no-shadow': 'warn',
-    'no-undef-init': 'error',
-    'no-unused-vars': 'warn',
-    'no-var': 'error',
-    'prefer-arrow-callback': ['error', { allowNamedFunctions: true }],
-    'prefer-const': 'error',
-    'quotes': ['warn', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
-    'semi': 'warn',
-  },
-  linterOptions: {
-    reportUnusedDisableDirectives: true,
-  },
-};
+import NetflixCommon from '@netflix/eslint-config';
 
 export default [
   {
+    ...NetflixCommon,
     files: ['**/*.js'],
     languageOptions: { globals: globals.browser },
-    ...common,
     ignores: [
       'server.js',
       'test.js',
@@ -40,14 +13,14 @@ export default [
     ],
   },
   {
+    ...NetflixCommon,
     files: ['demo/react/**/*.js'],
     languageOptions: { globals: { ...globals.browser, React: 'readonly', ReactDOM: 'readonly' } },
-    ...common,
   },
   {
+    ...NetflixCommon,
     files: ['server.js', 'test.js'],
     languageOptions: { globals: { ...globals.browser, ...globals.node } },
-    ...common,
   },
   {
     ignores: [
