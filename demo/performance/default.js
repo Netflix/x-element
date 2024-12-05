0501f507181d3ebf9e12b556149f5ccb003ddf26
Andrew Seier <andseier@gmail.com>
2024-12-04 20:31:10 -0800

    Improve our performance demo.
    There is still a fair amount of work to be done before we can be super
    confident in the results here, but this feels like a good step forward:
    
    * Tests all run in an iframe.
    * Progress bar is printed to improve feedback.
    * Long text moved to “PERFORMANCE.md”.

---
+++ demo/performance/default.js
@@ -0,0 +1,37 @@
+import { render, html } from '../../x-template.js';
+import CommonTest, { HtmlLiteralInterface } from './common.js';
+
+class Test extends CommonTest {
+  static id = 'default';
+
+  static async injectSetup() {
+    const properties = this.properties[0];
+    this.injectContext = { render, html, properties };
+  }
+  static injectRun() {
+    const { properties } = this.injectContext;
+    render(document.createElement('div'), HtmlLiteralInterface.getResultEval(html, properties));
+  }
+
+  static async initialSetup() {
+    const properties = this.properties[0];
+    this.initialContext = { render, html, properties };
+  }
+  static initialRun() {
+    const { properties } = this.initialContext;
+    render(document.createElement('div'), HtmlLiteralInterface.getResult(html, properties));
+  }
+
+  static async updateSetup() {
+    const container = document.createElement('div');
+    const getProperties = this.getProperties;
+    render(container, HtmlLiteralInterface.getResult(html, getProperties()));
+    this.updateContext = { render, html, container, getProperties };
+  }
+  static updateRun() {
+    const { container, getProperties } = this.updateContext;
+    render(container, HtmlLiteralInterface.getResult(html, getProperties()));
+  }
+}
+
+Test.initialize();
